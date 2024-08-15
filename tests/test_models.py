3bd82ca1d3c98cf63b6f2092450956c1ff12e706
Wilson B <wd.bracamonte10@gmail.com>
2024-08-14 21:32:13 -0400

    fixixing he coverage to 95%

--- tests/test_models.py
+++ tests/test_models.py
@@ -27,7 +27,7 @@ import os
 import logging
 import unittest
 from decimal import Decimal
-from service.models import Product, Category, db
+from service.models import Product, Category, db,DataValidationError
 from service import app
 from tests.factories import ProductFactory
 
@@ -137,6 +137,13 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "testing")
 
+    def test_update_a_excepion(self):
+        """It should throw an exception if you are trying to update a not created equipment"""
+        product = ProductFactory()
+        product.id = None
+        with self.assertRaises(DataValidationError) as asserterror:
+            product.update()
+       
     def test_delete_a_product(self):
         """It should Delete a Product"""
         product = ProductFactory()
@@ -193,3 +200,16 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+
+    def test_find_by_price(self):
+        """It should Find Products by price"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
